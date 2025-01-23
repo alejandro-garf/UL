7d3560795248d226f324a9877fd0b5fc29e2ed3c
Gael Alejandro Fonseca <gfon@csu.fullerton.edu>
2025-01-22 23:04:12 -0800

    Create a test API endpoint

---
+++ backend/src/main/java/com/undoculink/HelloController.java
@@ -0,0 +1,13 @@
+package com.undoculink;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/api/hello")
+    public String sayHello() {
+        return "Hello from the Spring Boot backend!";
+    }
+}

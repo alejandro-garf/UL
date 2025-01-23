2785626968a418877a03fee98d209ce1e9246fcb
Gael Alejandro Fonseca <gfon@csu.fullerton.edu>
2025-01-22 22:51:04 -0800

    created springboot backend

---
+++ backend/src/main/java/com/undoculink/UndoculinkApplication.java
@@ -0,0 +1,13 @@
+package com.undoculink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UndoculinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UndoculinkApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/undoculink/UndoculinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.undoculink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UndoculinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
